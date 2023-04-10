5fe796fa2f3568097c3e9c97e926d188b8c6a4a2
sebamendo <109764839+sebamendo@users.noreply.github.com>
2023-04-09 23:53:17 -0300

    Agregado estilo al filtrar, agregadas imágenes
    se agrego un poco de estilo al filtro de precios, se agregaron imágenes a dichos productos

--- js/scripts.js
+++ js/scripts.js
@@ -62,20 +62,21 @@ class Producto {
   }
 }
 
-const producto1 = new Producto(1, 'VolksWagen', 'Golf', 2015, 300, 'deportivo', 'https://via.placeholder.com/150');
-const producto2 = new Producto(2, 'Porsche', '911', 2020, 700, 'deportivo', 'https://via.placeholder.com/150');
-const producto3 = new Producto(3, 'Porsche', '718 Cayman', 2020, 800, 'deportivo', 'https://via.placeholder.com/150');
-const producto4 = new Producto(4, 'BMW', 'X1', 2020, 500, 'camioneta', 'https://via.placeholder.com/150');
-const producto5 = new Producto(5, 'BMW', 'X6', 2020, 500, 'camioneta', 'https://via.placeholder.com/150');
-const producto6 = new Producto(6, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://via.placeholder.com/150');
-const producto7 = new Producto(7, 'Mercedes', 'AMG C 63 S', 2021, 800, 'deportivo', 'https://via.placeholder.com/150');
-const producto8 = new Producto(8, 'BMW', 'MS Competition', 2017, 700, 'deportivo', 'https://via.placeholder.com/150');
-const producto9 = new Producto(9, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://via.placeholder.com/150');
-const producto10 = new Producto(10, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://via.placeholder.com/150');
-const producto11 = new Producto(11, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://via.placeholder.com/150');
-const producto12 = new Producto(12, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://via.placeholder.com/150');
-const producto13 = new Producto(13, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://via.placeholder.com/150');
-const producto14 = new Producto(14, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://via.placeholder.com/150');
+const producto1 = new Producto(1, 'VolksWagen', 'Golf', 2015, 300, 'deportivo', 'https://i0.wp.com/www.motorwebargentina.com/wp-content/uploads/2015/12/2016-Volkswagen-Golf-GTI-Motorweb-Argentina-01.jpg?fit=1289%2C857&ssl=1');
+const producto2 = new Producto(2, 'Porsche', '911', 2020, 700, 'deportivo', 'https://cdn-fastly.thetruthaboutcars.com/media/2022/07/20/9417385/review-2011-porsche-911-turbo-s-pdk.jpg?size=720x845&nocrop=1');
+const producto3 = new Producto(3, 'Porsche', '718 Cayman', 2020, 800, 'deportivo', 'https://hips.hearstapps.com/hmg-prod/images/2021-porsche-718-cayman-gt4-107-1616160477.jpg');
+const producto4 = new Producto(4, 'BMW', 'X1', 2020, 500, 'camioneta', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM0jDk2_qLCy4KkFqZ_XAK_rg3ygDZZ7fzVA&usqp=CAU');
+const producto5 = new Producto(5, 'BMW', 'X6', 2020, 500, 'camioneta', 'https://hips.hearstapps.com/hmg-prod/images/2020-bmw-x6-xdrive40i-206-1583153965.jpg?crop=0.616xw:0.692xh;0.344xw,0.308xh&resize=640:*');
+const producto6 = new Producto(6, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8y7ylyqtfKGtZh-kch9jSbfnWMslY9Wd4YQ&usqp=CAU');
+const producto7 = new Producto(7, 'Mercedes', 'AMG C 63 S', 2021, 800, 'deportivo', 'https://richmonds.com.au/wp-content/uploads/2019/04/Mercedes-amg-c63s-black-2.jpg');
+const producto8 = new Producto(8, 'BMW', 'MS Competition', 2017, 700, 'deportivo', 'https://cfx-wp-images.imgix.net/2022/04/2022-BMW-M3-Competition-Front-Exterior-Carter-MacLeod-Edit-scaled.jpg?auto=compress%2Cformat&ixlib=php-3.3.0&s=4fb1fd2208e9b5f753c0d833418d4016');
+const producto9 = new Producto(9, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8y7ylyqtfKGtZh-kch9jSbfnWMslY9Wd4YQ&usqp=CAU');
+const producto10 = new Producto(10, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8y7ylyqtfKGtZh-kch9jSbfnWMslY9Wd4YQ&usqp=CAU');
+const producto11 = new Producto(11, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8y7ylyqtfKGtZh-kch9jSbfnWMslY9Wd4YQ&usqp=CAU');
+const producto12 = new Producto(12, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8y7ylyqtfKGtZh-kch9jSbfnWMslY9Wd4YQ&usqp=CAU');
+const producto13 = new Producto(13, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8y7ylyqtfKGtZh-kch9jSbfnWMslY9Wd4YQ&usqp=CAU');
+const producto14 = new Producto(14, 'BMW', 'X3', 2017, 450, 'camioneta', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8y7ylyqtfKGtZh-kch9jSbfnWMslY9Wd4YQ&usqp=CAU');
+/* imagenes exclusivamente de muestra. todos los derechos reservados a quien corresponda */
 
 const listaProductos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9, producto10, producto11, producto12, producto13, producto14];
 
@@ -128,4 +129,3 @@ function generatePDF() {
   // Guarda el PDF como archivo para descarga
   doc.save('formulario.pdf');
 }
-
